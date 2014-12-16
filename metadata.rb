10521ddddafba668971d7b1bbd17a6a1cb9e48f1
Jeremy Olliver <jeremy.olliver@gmail.com>
2014-12-16 16:36:48 +1300

    Add source and issues url to metadata

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,8 @@ description      'Generates secure TLS config file'
 long_description 'Generates secure TLS config file for Apache/Nginx to the webserver root config at secure-ssl.conf'
 version          '0.1.0'
 
+source_url 'https://github.com/jeremyolliver/cookbook-ssl-config'
+issues_url 'https://github.com/jeremyolliver/cookbook-ssl-config/issues'
+
 recommends 'nginx'
 recommends 'apache2'
